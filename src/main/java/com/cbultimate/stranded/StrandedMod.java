bf7bdaae4437159ebc257fd53494327dd84d7035
CBUltimate <cb.cbultimate@gmail.com>
2017-01-13 23:15:04 +0800

    strandmod update 1

--- src/main/java/com/cbultimate/stranded/StrandedMod.java
+++ src/main/java/com/cbultimate/stranded/StrandedMod.java
@@ -4,6 +4,8 @@ import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 /**
  * Created by CBU on 13/1/2017.
@@ -16,12 +18,20 @@ public class StrandedMod {
     public static final String MODNAME = "Stranded Mod";
     private static final String LOGPREFIX = "["+MODNAME+"] >> ";
 
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        // some example code
         System.out.println(LOGPREFIX+ " Loading mod version:"+VERSION);
         System.out.println(LOGPREFIX+ " Mod made by CBUltimate.");
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
 
     }
 }
